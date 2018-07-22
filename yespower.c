0601edf9b74cf5ceee0cc08d6945913bc54bde97
nao20010128nao <nao20010128@gmail.com>
2018-07-22 15:50:34 +0900

    Update yespower.c

--- yespower.c
+++ yespower.c
@@ -39,7 +39,7 @@ int scanhash_yespower(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 		.version = YESPOWER_0_5,
 		.N = 2048,
 		.r = 8,
-		.pers = (const uint8_t *)endiandata,
+		.pers = "Client ",
 		.perslen = 10
 	};
         //we need bigendian data...
